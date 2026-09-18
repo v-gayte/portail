c348d5d6ff35feb6fe39f15c370f2dfab09a2232
v-gayte <virgil.gayte@gmail.com>
2026-09-18 14:10:38 +0200

    chore: setup SEO (robots, sitemap, hreflang, title) and sync english page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node"; // Importation de l'adaptateur Node
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,12 +12,21 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
+  site: "https://virgil-gayte.me",
   output: 'server', // Active le mode Server-Side Rendering (SSR)
   adapter: node({
     mode: "standalone", // Génère un serveur prêt à l'emploi
   }),
 
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), sitemap({
+    i18n: {
+      defaultLocale: 'fr',
+      locales: {
+        fr: 'fr',
+        en: 'en'
+      }
+    }
+  })],
 
   i18n: {
     defaultLocale: "fr",
